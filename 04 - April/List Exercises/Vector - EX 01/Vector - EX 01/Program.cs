129e470a43b8d20d82399479622f869abc5b1d67
gabrielsantos578 <santos.gabriel67018@gmail.com>
2023-04-20 14:48:05 -0300

    List of Exercises - Vector - Ex 01

---
+++ 04 - April/List Exercises/Vector - EX 01/Vector - EX 01/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        int[] vetor = new int[10];
+
+        Random numero = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            vetor[i] = numero.Next(99);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Vetor[" + i + "]: " + vetor[i]);
+        }
+        Console.WriteLine();
+
+        for (int i = 9; i > -1; i--)
+        {
+            Console.WriteLine("Vetor[" + i + "]: " + vetor[i]);
+        }
+
+    }
+
+};
---
+++ 04 - April/List Exercises/Vector - Ex 02/Vector - Ex 02/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        int[] vetor1 = new int[10];
+        int[] vetor2 = new int[10];
+
+        Random numero = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            vetor1[i] = numero.Next(99);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            vetor2[i] = vetor1[(9 - i)];
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Vetor1[" + i + "]: " + vetor1[i]);
+            Console.WriteLine("Vetor2[" + i + "]: " + vetor2[i]);
+        }
+
+    }
+
+};
